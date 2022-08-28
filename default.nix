b9e639a2dc9aa08daaa798df1f806717ab0ebf01
cyber-murmel <marble@chaoscomputer.club>
2022-08-28 18:54:52 +0200

    applied autoformatter

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{
-  pkgs ?
-  import (builtins.fetchGit {
-    name = "nixos-22.05-2022_08_27";
-    url = "https://github.com/nixos/nixpkgs/";
-    ref = "refs/heads/nixos-22.05";
-    rev = "f11e12ac6af528c1ba12426ce83cee26f21ceafd";
-  }) {}
+{ pkgs ? import
+    (builtins.fetchGit {
+      name = "nixos-22.05-2022_08_27";
+      url = "https://github.com/nixos/nixpkgs/";
+      ref = "refs/heads/nixos-22.05";
+      rev = "f11e12ac6af528c1ba12426ce83cee26f21ceafd";
+    })
+    { }
 }:
 
 pkgs.stdenv.mkDerivation {
